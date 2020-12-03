5c31073fba0317bc81db44112b97ae54668751d6
bwangelme <bwangel.me@gmail.com>
2020-12-03 22:55:56 +0800

    Feat: add get api

--- cmd/cli/cli.go
+++ cmd/cli/cli.go
@@ -3,45 +3,58 @@ package main
 
 import (
 	"context"
+	"flag"
+	"gRPCDemo/pb"
 	"log"
-	"os"
 	"time"
 
-	"gRPCDemo/pb"
-
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
-const (
-	address     = "localhost:50051"
-	defaultName = "nihao"
+var (
+	tls                = flag.Bool("tls", false, "Connection use TLS")
+	caFile             = flag.String("ca_file", "", "the file containing the ca root cert file")
+	serverAddr         = flag.String("server_addr", "localhost:10000", "Server Address")
+	serverHostOverride = flag.String("server_host_override", "dev.bwangel.abc", "The server name used to verify the hostname returned by the TLS handshake")
 )
+
+func printFeature(client pb.RouteGuideClient, point *pb.Point) {
+	log.Printf("Getting feature for point(%d, %d)", point.Latitude, point.Longitude)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	feature, err := client.GetFeature(ctx, point)
+	if err != nil {
+		log.Fatalf("%v.GetFeatures(_) = _, %v: ", client, err)
+	}
+	log.Println(feature)
+}
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	flag.Parse()
+	var opts []grpc.DialOption
+	if *tls {
+		if *caFile == "" {
+			*caFile = "/home/xuyundong/Certs/cacert.pem"
+		}
+		creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
+		if err != nil {
+			log.Fatalf("Failed to create TLC credentials %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+	}
+
+	opts = append(opts, grpc.WithBlock())
+	log.Printf("Start to dial with %v\n", *serverAddr)
+	conn, err := grpc.Dial(*serverAddr, opts...)
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Fatalf("failed to connect: %v", err)
 	}
 	defer conn.Close()
-	c := pb.NewGreeterClient(conn)
+	client := pb.NewRouteGuideClient(conn)
 
-	// Contact the server and print out its response.
-	name := defaultName
-	if len(os.Args) > 1 {
-		name = os.Args[1]
-	}
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting: %s", r.GetMessage())
-
-	r, err = c.SayHelloAgain(ctx, &pb.HelloRequest{Name: name})
-	if err != nil {
-		log.Fatalf("could not greet: %v", err)
-	}
-	log.Printf("Greeting: %s", r.GetMessage())
+	printFeature(client, &pb.Point{Latitude: 407838351, Longitude: -746143763})
 }
--- cmd/svc/svc.go
+++ cmd/svc/svc.go
@@ -1,43 +1,111 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net"
+	"sync"
 
 	"gRPCDemo/pb"
 
+	"context"
+
+	"github.com/golang/protobuf/proto"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/status"
 )
 
-const (
-	port = ":50051"
+var (
+	tls        = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile   = flag.String("cert_file", "", "The TLS Cert file")
+	keyFile    = flag.String("key_file", "", "The TLS Key file")
+	jsonDBFile = flag.String("json_db_file", "", "A json file containing a list of features")
+	port       = flag.Int("port", 10000, "The Server port")
 )
 
-// server is used to implement helloworld.GreeterServer.
-type server struct {
-	pb.UnimplementedGreeterServer
+type routeGuideServer struct {
+	pb.UnimplementedRouteGuideServer
+	savedFeatures []*pb.Feature
+	mu            sync.Mutex
+	routeNodes    map[string][]*pb.RouteNode
 }
 
-// SayHello implements helloworld.GreeterServer
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.Name)
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+func (s *routeGuideServer) GetFeature(ctx context.Context, point *pb.Point) (*pb.Feature, error) {
+	for _, feature := range s.savedFeatures {
+		if proto.Equal(feature.Location, point) {
+			return feature, nil
+		}
+	}
+
+	return &pb.Feature{Location: point}, nil
+}
+func (s *routeGuideServer) ListFeatures(rect *pb.Rectangle, stream pb.RouteGuide_ListFeaturesServer) error {
+	return status.Errorf(codes.Unimplemented, "method ListFeatures not implemented")
+}
+func (s *routeGuideServer) RecordRoute(stream pb.RouteGuide_RecordRouteServer) error {
+	return status.Errorf(codes.Unimplemented, "method RecordRoute not implemented")
+}
+func (s *routeGuideServer) RouteChat(stream pb.RouteGuide_RouteChatServer) error {
+	return status.Errorf(codes.Unimplemented, "method RouteChat not implemented")
 }
 
-func (s *server) SayHelloAgain(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.Name)
-	return &pb.HelloReply{Message: "Hello " + in.Name}, nil
+func (s *routeGuideServer) loadFeatures(filename string) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Read file %v failed", filename)
+	}
+
+	if err := json.Unmarshal(data, &s.savedFeatures); err != nil {
+		log.Fatalf("Failed to load default features: %v", err)
+	}
+
+	log.Printf("Load %d features from json db\n", len(s.savedFeatures))
+}
+
+func newServer() *routeGuideServer {
+	s := &routeGuideServer{
+		routeNodes: make(map[string][]*pb.RouteNode),
+	}
+	s.loadFeatures(*jsonDBFile)
+	return s
 }
 
 func main() {
-	lis, err := net.Listen("tcp", port)
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
-	if err := s.Serve(lis); err != nil {
+	var opts []grpc.ServerOption
+	if *tls {
+		if *certFile == "" {
+			*certFile = "/home/xuyundong/Certs/dev.bwangel.abc.pem"
+		}
+
+		if *keyFile == "" {
+			*keyFile = "/home/xuyundong/Certs/dev.bwangel.abc.key"
+		}
+
+		cerds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalln("Failed to generate crendentials", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(cerds)}
+	}
+
+	if *jsonDBFile == "" {
+		*jsonDBFile = "./testdata/route_guide_db.json"
+	}
+
+	server := grpc.NewServer(opts...)
+	log.Printf("Listening on the %v\n", *port)
+	pb.RegisterRouteGuideServer(server, newServer())
+	if err := server.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
